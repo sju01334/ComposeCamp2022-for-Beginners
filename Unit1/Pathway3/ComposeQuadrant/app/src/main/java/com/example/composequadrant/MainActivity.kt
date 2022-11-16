b76f531d40ced17239213e31ac79e3526e1a416c
mijinSong` <sju01334@gmail.com>
2022-11-16 23:08:51 +0900

    Compose Quadrant 완료

--- Unit1/Pathway3/ComposeQuadrant/app/src/main/java/com/example/composequadrant/MainActivity.kt
+++ Unit1/Pathway3/ComposeQuadrant/app/src/main/java/com/example/composequadrant/MainActivity.kt
@@ -5,14 +5,11 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -22,15 +19,46 @@ import com.example.composequadrant.ui.theme.ComposeQuadrantTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent { }
+        setContent {
+            ComposeQuadrantTheme {
+                ComposeQuadrantApp()
+            }
+        }
     }
 }
 
 @Composable
 fun ComposeQuadrantApp() {
     Column() {
-        Row() { }
-        Row() { }
+        Row(modifier = Modifier.weight(1f)) {
+            ComposableInfoCard(
+                "Text composable",
+                "Displays text and follows Material Design guidelines.",
+                Color.Green,
+                Modifier.weight(1f)
+
+            )
+            ComposableInfoCard(
+                "Image composable",
+                "Creates a composable that lays out and draws a given Painter class object.",
+                Color.Yellow,
+                Modifier.weight(1f)
+            )
+        }
+        Row(modifier = Modifier.weight(1f)) {
+            ComposableInfoCard(
+                "Row composable",
+                "A layout composable that places its children in a horizontal sequence.",
+                Color.Cyan,
+                Modifier.weight(1f)
+            )
+            ComposableInfoCard(
+                "Column composable",
+                "A layout composable that places its children in a vertical sequence.",
+                Color.LightGray,
+                Modifier.weight(1f)
+            )
+        }
     }
 }
 
@@ -41,10 +69,32 @@ private fun ComposableInfoCard(
     backgroundColor: Color,
     modifier: Modifier = Modifier
 ) {
-    Column( ) { }
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier
+            .fillMaxHeight()
+            .background(backgroundColor)
+            .padding(16.dp)
+    ) {
+        Text(
+            text = title,
+            modifier = Modifier
+                .padding(bottom = 16.dp),
+            fontWeight = FontWeight.Bold
+        )
+        Text(
+            text = description,
+            modifier = Modifier
+                .fillMaxWidth(),
+            textAlign = TextAlign.Justify
+        )
+    }
 }
 
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() { }
+fun DefaultPreview() {
+    ComposeQuadrantApp()
+}
